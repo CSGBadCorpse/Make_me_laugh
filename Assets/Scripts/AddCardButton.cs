ae1cf499ef91a1080c140def50e6849dd9025a75
guchengyu <imbagj@yeah.net>
2024-01-27 12:02:42 +0800

    Wordbox update.

--- Assets/Scripts/AddCardButton.cs
+++ Assets/Scripts/AddCardButton.cs
@@ -6,14 +6,12 @@ using UnityEngine.UI;
 public class AddCardButton : MonoBehaviour
 {
     public List<CardInfo> cardInfoList;
-    private CardHand cardHand;
     private void Awake() {
-        cardHand = UIManager.Instance.cardHand;
     }
     public void onClicked()
     {
         //Random add a card to cardHand
         int randomCardIndex = Random.Range(0, cardInfoList.Count);
-        cardHand.AddCard(cardInfoList[randomCardIndex]);
+        UIManager.Instance.cardHand.AddCard(cardInfoList[randomCardIndex]);
     }
 }
---
+++ Assets/Scripts/WordBox/WordBox.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordBox : MonoBehaviour
+{
+    private TextMeshProUGUI wordBox;
+    private Scrollbar scrollbar;
+    private string text
+    {
+        get => currentShowingText;
+        set
+        {
+            currentShowingText = value;
+            wordBox.text = currentShowingText;
+            StartCoroutine(ScrollToBottom());
+        }
+    }
+    private string currentShowingText;
+    
+    private void Awake()
+    {
+        wordBox = transform.Find("Scroll View/Viewport/Word Box").GetComponent<TextMeshProUGUI>();
+        scrollbar = transform.Find("Scroll View/Scrollbar Vertical").GetComponent<Scrollbar>();
+    }
+    private void Start() {
+        text = "hahahahahahaha\n";
+    }
+
+    private IEnumerator ScrollToBottom()
+    {
+        bool isFinished = false;
+        if (isFinished)
+        {
+            yield break;
+        }
+
+        float lerpTimer = 0;
+        while(lerpTimer<=1)
+        {
+            scrollbar.value = Mathf.Lerp(scrollbar.value,0,lerpTimer);
+            lerpTimer+=Time.deltaTime * 1;
+            yield return null;
+        }
+        isFinished = true;
+    }
+
+
+    public void GivenCardUsed(Card card)
+    {
+        SetNewWord(card.cardInfo.CardFunnyWords);
+    }
+    public void ClearWord()
+    {
+        text = "";
+    }
+    public void SetNewWord(string word)
+    {
+        text += word + "\n";
+    }
+}
